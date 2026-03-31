753070ec662a96a0a145f9e29e622fb8f542cf39
m-montefalcon <mmontefalconschlprps@gmail.com>
2026-03-31 23:19:35 +0800

    Setup: MongoDB Mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './modules/users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import Joi from 'joi';
+import { MongooseModule } from '@nestjs/mongoose';
+import { getMongoConfig } from './config/database.config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validationSchema: Joi.object({
+        MONGO_URI: Joi.string().required(),
+        PORT: Joi.number().default(3000),
+      }),
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getMongoConfig,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
